a1e87dd7f7f89da4e4f507c5caa8d9cc2f5befc0
Ivan Akkuratov <jojowoodo@yahoo.com>
2020-05-11 14:00:00 +0300

    Adds circles to lineCSV

--- builds/d3/lineCSV.js
+++ builds/d3/lineCSV.js
@@ -8,16 +8,54 @@ function drawChart() {
     });*/
     
     d3.csv("lineData.csv").then(function(data) {
-        console.log(data);
 
         //ladda in datan
-        var temps = [], months = [];
+        var temps = [], months = [], dataFix=[];
         for(i=0; i<data.length; i++) {
             months.push(data[i].Month);
-            temps.push(data[i].Temp);
+            temps.push( parseFloat(data[i].Temp))
+            dataFix.push({ month:months[i], temp:temps[i]} );
         }
         
+        console.log(dataFix);
         console.log(temps);
         console.log(months);
+
+        //skapa ritunderlag
+        var width = 600, height = 300;
+        var canvas = d3.select('body').append('svg').attr('width',width).attr('height',height);
+
+        var xScale = d3.scaleBand()
+            .domain(months)
+            .range([0,width]);
+
+        //Temperaturen -20 kan inte användas som Y axel, -20 är ju utanför canvas!
+        //Vi behöver en skala, för temperatur passar en lineär skala
+
+        yScale = d3.scaleLinear()
+        .domain([d3.min(temps), d3.max(temps)]) //Vilka värden ska konverteras till pixelvärden
+        .range([height, 0]);  //Skala över vilken pixelstorlek
+
+        //generera d strängen för path
+        var dString = d3.line()
+        .x(function(d){ return d.month})
+        .y(function(d){ return yScale(d.temp)});
+        //console.log(dString(data));
+        
+        var dotsGroup = canvas.append('g');
+
+        //rita linjen
+        canvas.append('path')
+        .attr('fill','none')
+        .attr('stroke','blue')
+        .attr('d', dString(dataFix));
+
+
+        canvas.selectAll('dots').data(dataFix)
+    .enter()
+    .append('circle')
+    .attr('cx', function(d) {return xScale(d.month) })
+    .attr('cy',function(d)  {return  yScale(d.temp)} )
+    .attr('r','2')
     });
-};
+}
--- builds/d3/lineJSON.js
+++ builds/d3/lineJSON.js
@@ -8,7 +8,7 @@ function drawChart() {
         //en array med våra x och y kordinater för cassiopeia
         var dataArray = data;
 
-    var width = 500, height = 300;
+                var width = 500, height =300;
 
         //Skapa arrays för att lagra x och y värden
     var xs = [];
@@ -24,14 +24,24 @@ function drawChart() {
 
     //d.3line() är en generator som genererar en sträng för d="M x y..."
     var path = d3.line()
-        .x( function(data) {return data.x * 16} )
-        .y( function(data) {return data.y * 16} )
-        .curve(d3.curveCardinal);
+        .x( function(data) {return data.x * 6} )
+        .y( function(data) {return data.y * 6} );
+        //.curve(d3.curveCardinal);
 
     //rita en linje (obs path inte svg line)
     canvas.append("path")
     .attr('fill', 'none')
     .attr('stroke','blue')
     .attr('d',path(dataArray));
+
+    var dotsGroup = canvas.append('g');
+
+    canvas.selectAll('dots').data(dataArray)
+    .enter()
+    .append('circle')
+    .attr('cx', function(data) {return data.x *6 })
+    .attr('cy',function(data) {return data.y *6 } )
+    .attr('r','2')
+
 });
 }
