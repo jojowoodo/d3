b383ec258ac9850e0bef907a03c5b3b039a97892
Ivan Akkuratov <jojowoodo@yahoo.com>
2020-05-15 13:15:57 +0300

    adds new capabilities

--- builds/d3/lineCSV.js
+++ builds/d3/lineCSV.js
@@ -22,40 +22,56 @@ function drawChart() {
         console.log(months);
 
         //skapa ritunderlag
-        var width = 600, height = 300;
+        var width = 1000, height = 200, margin = 20;
+        var chartWidth = width-(margin*2), chartHeight = height-(margin*2);
         var canvas = d3.select('body').append('svg').attr('width',width).attr('height',height);
 
         var xScale = d3.scaleBand()
             .domain(months)
-            .range([0,width]);
+            .range([0,chartWidth]);
 
         //Temperaturen -20 kan inte användas som Y axel, -20 är ju utanför canvas!
         //Vi behöver en skala, för temperatur passar en lineär skala
 
-        yScale = d3.scaleLinear()
+        var yScale = d3.scaleLinear()
         .domain([d3.min(temps), d3.max(temps)]) //Vilka värden ska konverteras till pixelvärden
-        .range([height, 0]);  //Skala över vilken pixelstorlek
+        .range([chartHeight, 0]);  //Skala över vilken pixelstorlek
 
         //generera d strängen för path
         var dString = d3.line()
-        .x(function(d){ return d.month})
-        .y(function(d){ return yScale(d.temp)});
+        .x(function(d){ return xScale(d.month) })
+        .y(function(d){ return yScale(d.temp) });
         //console.log(dString(data));
         
-        var dotsGroup = canvas.append('g');
+        //Y axeln för tempereturer
+       var yAxis = d3.axisLeft(yScale);
+
+       //X axeln för månader
+       var xAxis = d3.axisBottom(xScale);
+
+       //skapa en grupp som container för grafen
+    var chartGroup = canvas.append('g').attr("transform", "translate("+margin+","+margin+")");
 
         //rita linjen
-        canvas.append('path')
+        chartGroup.append('path')
         .attr('fill','none')
         .attr('stroke','blue')
-        .attr('d', dString(dataFix));
+        .attr('d', dString(dataFix))
+        .attr("transform","translate("+chartWidth/months.length/2+",0)");
 
-
-        canvas.selectAll('dots').data(dataFix)
+        
+//lägg till punkter (cirklar) till  datapunkterna
+    chartGroup.selectAll('dots').data(dataFix)
     .enter()
     .append('circle')
-    .attr('cx', function(d) {return xScale(d.month) })
-    .attr('cy',function(d)  {return  yScale(d.temp)} )
+    .attr('cx', function(d) {return xScale(d.month)})
+    .attr('cy',function(d)  {return yScale(d.temp)})
     .attr('r','2')
+    .attr("transform","translate("+chartWidth/months.length/2+",0)");
+   
+
+    //Rita axlar genom att .call'a på dem
+    chartGroup.append("g").call(yAxis);
+    chartGroup.append("g").call(xAxis).attr("transform","translate(0,"+chartHeight+")");
     });
 }
